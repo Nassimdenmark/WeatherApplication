6cd636600581dfb123e28b4ead0505ba05a76592
Nassimdenmark <55272745+Nassimdenmark@users.noreply.github.com>
2021-07-05 15:35:39 +0200

    Changed the weatherModel Connected to the api

--- WeatherApplication/Controllers/HomeController.cs
+++ WeatherApplication/Controllers/HomeController.cs
@@ -1,30 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Newtonsoft.Json;
+using System;
 using System.Web.Mvc;
+using WeatherApplication.Models;
+using WeatherApplication.Utils;
 
 namespace WeatherApplication.Controllers
 {
     public class HomeController : Controller
     {
+        readonly string key = "x-rapidapi-key:,e6fdd2cc99mshaa41acf362a7e09p172484jsn4c75f592c564";
+        readonly string host = "x-rapidapi-host:,community-open-weather-map.p.rapidapi.com";
+
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            var param = "aarhus,dkk";
+            var units = "metric";
+            var url = String.Format($"https://community-open-weather-map.p.rapidapi.com/forecast?q={param}?units={units}");
+            var request = HelperClass.WbRequest(url, "GET");
+            HelperClass.InsertHead(request, key, host);
+            var response = HelperClass.GetHttpResponse(request);
+            var _weatherModel = JsonConvert.DeserializeObject<WeatherModel>(response);
+            return View(_weatherModel);
         }
     }
 }
---
+++ WeatherApplication/Utils/HelperClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace WeatherApplication.Utils
+{
+    public class HelperClass
+    {
+        public static HttpWebRequest WbRequest(string shopAddress, string methodType)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(shopAddress);
+            request.Method = methodType;
+            return request;
+        }
+
+        public static string GetHttpResponse(HttpWebRequest request)
+        {
+            string responseString = "";
+            try
+            {
+                var response = (HttpWebResponse)request.GetResponse();
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    using (var stream = response.GetResponseStream())
+                    {
+                        using (var reader = new StreamReader(stream))
+                        {
+                            responseString = reader.ReadToEnd();
+                        }
+                    }
+                }
+                return responseString;
+            }
+            catch (Exception ex) { return ex.Message; }
+        }
+
+        public static void InsertHead(HttpWebRequest request, string _key, string _host)
+        {
+            var key = _key.Split(',');
+            var host = _host.Split(',');
+            request.Headers.Add(key[0] + key[1]);
+            request.Headers.Add(host[0] + host[1]);
+        }
+    }
+}
